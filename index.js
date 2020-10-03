bbc91d367e54debf4e492efe749d6556149ff7ca
vladimir <lovkiy2012@gmail.com>
2020-10-03 15:44:14 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const model = [
+  {
+    type: 'title',
+    value: 'Hello world from js',
+  },
+  { type: 'text', value: 'here we go with some text' },
+  { type: 'columns', value: ['111111111111', '222222222222', '333333333333'] },
+];
+
+const $site = document.querySelector('#site');
