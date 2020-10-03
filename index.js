96f3b80fd1a2dc9e10ad74fd79c2e62319105d5c
vladimir <lovkiy2012@gmail.com>
2020-10-03 16:15:33 +0300

    creating model

--- index.js
+++ index.js
@@ -8,3 +8,49 @@ const model = [
 ];
 
 const $site = document.querySelector('#site');
+
+model.forEach((block) => {
+  console.log(block);
+
+  let html = '';
+
+  if (block.type === 'title') {
+    html = `
+      <div class="row">
+      <div class="col-sm"><h1>${block.value}</h1></div>
+    </div>
+      `;
+  } else if (block.type === 'text') {
+    html = `
+      <div class="row">
+          <div class="col-sm">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto autem sit aperiam
+              consequuntur.
+            </p>
+          </div>
+        </div>
+      `;
+  } else if (block.type === 'columns') {
+    html = `
+      <div class="row">
+          <div class="col-sm">
+            <p>
+              ${block.value[0]}
+            </p>
+          </div>
+          <div class="col-sm">
+            <p>
+            ${block.value[1]}
+            </p>
+          </div>
+          <div class="col-sm">
+            <p>
+            ${block.value[2]}
+            </p>
+          </div>
+        </div>
+      `;
+  }
+  $site.insertAdjacentHTML('beforeend', html);
+});
