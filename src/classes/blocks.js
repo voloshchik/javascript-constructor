790471c015ff5f0845f24691ea314269c0f24253
vladimir <lovkiy2012@gmail.com>
2020-10-04 11:00:48 +0300

    improve class Block

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,28 +1,49 @@
+import { col, row, css } from '../utils';
+
 class Block {
-  constructor(type, value, options) {
-    this.type = type;
+  constructor(value, options) {
     this.value = value;
     this.options = options;
+  }
+  toHtml() {
+    throw new Error('Метод toHTML должен реализован!!!');
   }
 }
 
 export class TitleBlock extends Block {
   constructor(value, options) {
-    super('title', value, options);
+    super(value, options);
+  }
+
+  toHtml() {
+    const { tag = h1, styles } = this.options;
+
+    return row(col(`<${tag}>${this.value}</${tag}>`), css(styles));
   }
 }
 export class ImageBlock extends Block {
   constructor(value, options) {
-    super('image', value, options);
+    super(value, options);
+  }
+  toHtml() {
+    const { styles, alt = '', imageStyles: is } = this.options;
+    return row(`<img alt="${alt}" style="${css(is)}" src='${this.value}' />`, css(styles));
   }
 }
 export class ColumnsBlock extends Block {
   constructor(value, options) {
-    super('columns', value, options);
+    super(value, options);
+  }
+  toHtml() {
+    const html = this.value.map(col).join('');
+    return row(html, css(this.options.styles));
   }
 }
 export class TextBlock extends Block {
   constructor(value, options) {
-    super('text', value, options);
+    super(value, options);
+  }
+  toHtml() {
+    return row(col(`<p>${this.value}</p>`), css(this.options.styles));
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './styles/main.css';
 const $site = document.querySelector('#site');
 
 model.forEach((block) => {
-  const toHTML = templates[block.type];
-  if (toHTML) {
-    $site.insertAdjacentHTML('beforeend', toHTML(block));
-  }
+  // const toHTML = templates[block.type];
+
+  $site.insertAdjacentHTML('beforeend', block.toHtml());
 });
