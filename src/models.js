03c924603278607993027bcef71fc54063c01626
vladimir <lovkiy2012@gmail.com>
2020-10-04 10:37:36 +0300

    TitleBlock

--- src/models.js
+++ src/models.js
@@ -1,8 +1,8 @@
 import image from '../assets/image.png';
-import { Block } from './classes/blocks';
+import { TitleBlock, ImageBlock, ColumnsBlock, TextBlock } from './classes/blocks';
 
 export const model = [
-  new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+  new TitleBlock('Конструктор сайтов на чистом JavaScript', {
     tag: 'h2',
     styles: {
       background: 'linear-gradient(to right, #ff0099, #493240)',
@@ -12,7 +12,7 @@ export const model = [
     },
   }),
 
-  new Block('text', 'Конструктор сайтов на чистом JavaScript', {
+  new TextBlock('Конструктор сайтов на чистом JavaScript', {
     styles: {
       background: 'linear-gradient(to left, #f2994a, #f2c94c)',
       padding: '1rem',
@@ -20,7 +20,7 @@ export const model = [
     },
   }),
 
-  new Block('columns', ['111111111111', '222222222222', '333333333333'], {
+  new ColumnsBlock(['111111111111', '222222222222', '333333333333'], {
     styles: {
       background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
       padding: '2rem',
@@ -28,7 +28,7 @@ export const model = [
       'font-weight': 'bold',
     },
   }),
-  new Block('image', image, {
+  new ImageBlock(image, {
     styles: {
       padding: '2rem 0',
       display: 'flex',
@@ -40,7 +40,7 @@ export const model = [
     },
     alt: 'Это картинка',
   }),
-  new Block('text', 'Конструктор сайтов на чистом JavaScript', {
+  new TextBlock('Конструктор сайтов на чистом JavaScript', {
     styles: {
       background: 'linear-gradient(to left, #f2994a, #f2c94c)',
       padding: '1rem',
